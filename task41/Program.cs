f56530bb45e4439f78e9d9451e13c2f4cf3edbff
Anna <anna.skorohodova@inbox.ru>
2022-10-21 00:31:42 +0300

    ДЗ 6

---
+++ task41/Program.cs
@@ -0,0 +1,39 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+CountPositive(GetValue("Введите числа через запятую"));
+
+string[] GetValue(string message)
+{
+  Console.WriteLine(message);
+  string input = Console.ReadLine();
+  string[] inputArray = input.Split(new char[] { ',' });
+  return inputArray;
+}
+
+void CountPositive (string[] numbers)
+{
+  int count = 0;
+  for (int i = 0; i < numbers.Length; i++)
+  {
+    if (CheckNumbers(numbers[i]))
+    {
+      if (int.Parse(numbers[i]) > 0)
+      {
+        count++;
+      }
+    }
+  }
+  Console.WriteLine("Количество положительных чисел " + count);
+}
+
+bool CheckNumbers(string num) //проверка на число
+{
+  int temp;
+  if (int.TryParse(num, out temp))
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
---
+++ task43/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+//Console.WriteLine("Hello, World!");
+
+int b1Input = GetValue("Введите b1");
+int k1Input = GetValue("Введите k1");
+int b2Input = GetValue("Введите b2");
+int k2Input = GetValue("Введите k2");
+
+double x = CalculateX(b1Input, k1Input, b2Input, k2Input);
+double y = CalculateY(x, b1Input, k1Input);
+
+Console.WriteLine($"({x}; {y})");
+
+int GetValue (string message)
+{
+  Console.WriteLine(message);
+  int value = int.Parse(Console.ReadLine());
+  return value;
+}
+
+double CalculateX(int b1, int k1, int b2, int k2)
+{
+  double x1 = k1 - k2;
+  double x2 = b2 - b1;
+  double x = x2 / x1;
+  //double x = (b2 - b1) / (k1 - k2); Не понимаю, почему так получается x = 0
+  return x;
+}
+
+double CalculateY (double x, int b1, int k1)
+{
+  double y = k1 * x + b1;
+  return y;
+}
